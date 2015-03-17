4a6f6463869719d1c25f26f5a6c12454938db083
Morten Liland <morten.liland@gmail.com>
2015-03-17 20:46:17 +0100

    added server3.py, client3.py and updated serial_test.ino

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -9,20 +9,22 @@ void setup(){
 void loop (){
   if(stringComplete){
     Serial.println(inputString);
-    
     inputString = "";
     stringComplete = false;
   }
 }
 
 void serialEvent(){
-  while(Serial.available()){
-    char inChar = (char) Serial.read();
-    inputString += inChar;
-    
+  while(Serial.available()>0){
+    char inChar= (char) Serial.read();
+    if (inChar == '\r' || inChar == '\n'){
+      stringComplete = true; 
+    }
+    else{
+      inputString += inChar;
+    }
 //    if(inChar ==  '\n'){
 //       stringComplete = true;
 //    }
   }
-  stringComplete = true;
 }
